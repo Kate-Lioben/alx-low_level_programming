7aac1832304e9c15f066f6783e43b1d0c079b312
Kate-Lioben <benabaiden@gmail.com>
2022-09-09 11:37:04 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,37 @@
-#include<stdio.h>
+/*
+
+ * File: 5-printf.c
+
+ * Auth: kate kamau
+
+ */
+
+
+
+#include <stdio.h>
 
 
 
 /**
 
- * main - Entry point
+ * main - Prints "with proper grammar, but the outcome is
+
+ *                a piece of art," followed by a new line.
 
  *
 
- * Return: Always 0 (Success)
+ * Return: Always 0.
 
  */
 
 int main(void)
-
+  
 {
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
 
-puts("\"Programming is like building a multilingual puzzle");
-
-
-
-return (0);
-
+  
+  return (0);
+  
 }
