5d7bf7374b6b50f6e9170690f35e4e1ff4631d8f
Kate-Lioben <benabaiden@gmail.com>
2022-09-09 09:49:01 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,39 @@
+
+
+/*
+
+ * File: 5-printf.c
+
+ * Auth: kate kamau
+
+ */
+
+
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Prints "with proper grammar, but the outcome is
+
+ *                a piece of art," followed by a new line.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+return (0);
+
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-print exactly "Programming is like building a multilingual puzzle
+with proper grammar, but the outcome is a piece of art
