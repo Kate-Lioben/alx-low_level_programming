6df729bf988f3f056c4c7af4fb3db6456d6dd7e2
Kate-Lioben <benabaiden@gmail.com>
2022-09-13 05:07:47 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+
+
+/**
+ * prints _putchar
+ */
+
+int main(void)
+{
+	char=ch
+		ch=_putchar;
+
+		putchar(ch);
+		{
+                
+			putchar('/n')
+		}
+
+
+} 
+
+return(0)
+
